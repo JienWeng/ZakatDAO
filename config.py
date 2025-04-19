339c1888665b161115e7f2194c97c79823127af7
Jien Weng <reallyhat@gmail.com>
2025-04-19 19:24:31 +0800

    update pitch deck

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # Zakat types configuration
 ZAKAT_TYPES = [
-    {"id": 1, "name": "Zakat Pendapatan", "description": "Income Zakat"},
-    {"id": 2, "name": "Zakat Perniagaan", "description": "Business Zakat"},
-    {"id": 3, "name": "Zakat Simpanan", "description": "Savings Zakat"},
+    {"id": 1, "name": "Zakat Harta", "description": "Wealth Zakat"},
+    # {"id": 2, "name": "Zakat Perniagaan", "description": "Business Zakat"},
+    # {"id": 3, "name": "Zakat Harta", "description": "Savings Zakat"},
 ]
 
 ZAKAT_TYPES_BY_ID = {t["id"]: t for t in ZAKAT_TYPES}
